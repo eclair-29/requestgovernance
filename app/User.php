e37bb04d1039f3ac361523bd551664fa631dffbe
Miguel D <dmgzky@gmail.com>
2023-12-18 16:46:18 +0800

    add models

--- app/User.php
+++ app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +36,26 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        // todo: translate below eloquent query to raw sql query
+        return $this->roles->where('role_code', $role)->count() > 0;
+    }
+
+    public function hasAnyRole($roles)
+    {
+        foreach ($roles as $role) {
+            if ($this->hasRole($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
