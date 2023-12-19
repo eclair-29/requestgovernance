b0cfa13a4e0b6534346f41f66390b2b3bb355151
Miguel D <dmgzky@gmail.com>
2023-12-19 16:39:47 +0800

    Add files via upload

---
+++ resources/recentlogs.blade.php
@@ -0,0 +1,10 @@
+<div class='{{ $col }}'>
+  <div class="card">
+    <div class="card-header">
+      {{ $slot }}
+    </div>
+    <div class="card-body">
+      <p>Activity Logs (for past 3 Months)</p>
+    </div>
+  </div>
+</div>
