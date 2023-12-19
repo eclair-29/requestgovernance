7e63027b3264094d9ed8ca1d07c85c0229041e0a
Miguel D <dmgzky@gmail.com>
2023-12-19 16:38:27 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
+/* Sir Migz Routes */
+/* requests (data list) route */
+Route::get('/', 'ServiceRequestController@index');
+Route::get('/requests', 'ServiceRequestController@create');
+Route::get('/requests/{id}/edit', 'ServiceRequestController@edit');
+Route::get('/requests/{id}', 'ServiceRequestController@show');
+
+/* Sir Seus Routes */
+Route::get('dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('Admin', function () {
+    return view('rnd.AdminChoose');
+});
+
+Route::get('OnlineMIS', function () {
+    return view('rnd.request');
+});
+
+Route::get('global', function () {
+    return view('rnd.global');
+});
+
+Route::get('local', function () {
+    return view('rnd.local');
+});
+
+Route::get('globalRequest', function () {
+    return view('rnd.GlobalRequest');
+});
+
+Route::get('localRequest', function () {
+    return view('rnd.LocalRequest');
 });
